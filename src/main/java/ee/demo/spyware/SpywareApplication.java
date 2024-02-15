79ff23d859539afdad59bf8930f009e71deb05eb
Markus Leemet <markusleemet@gmail.com>
2024-02-15 20:49:37 +0200

    Create SpringBoot project structure

---
+++ src/main/java/ee/demo/spyware/SpywareApplication.java
@@ -0,0 +1,13 @@
+package ee.demo.spyware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpywareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpywareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/demo/spyware/SpywareApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.demo.spyware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpywareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
