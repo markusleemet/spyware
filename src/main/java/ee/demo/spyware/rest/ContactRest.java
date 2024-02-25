551a22e9096f10641c00b985f7505edb6597794b
Markus Leemet <markusleemet@gmail.com>
2024-02-25 19:38:46 +0200

    Update CORS policy

--- src/main/java/ee/demo/spyware/rest/ContactRest.java
+++ src/main/java/ee/demo/spyware/rest/ContactRest.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
 @RestController
-@CrossOrigin(origins = {"http://127.0.0.1:5173", "https://spyware.leemetmarkus.ee"}, allowedHeaders = "*")
+@CrossOrigin(origins = {"http://127.0.0.1:5173", "https://spyware.leemetmarkus.ee"}, allowedHeaders = "*", methods = {DELETE, GET, POST, PUT, OPTIONS})
 @RequestMapping("/contact")
 @RequiredArgsConstructor
 public class ContactRest {
